b5bad343949a99b3da0ba6d035f63a3a29e2af34
Markus Mannheim <markus@mannheim.media>
2021-05-09 23:16:09 +1000

    update

--- resources/suburbFunctions.js
+++ resources/suburbFunctions.js
@@ -29,6 +29,13 @@ function mapShapes() {
       })
       .on("click", suburbInfo);
 
+  suburbLabels = labelDiv
+    .selectAll(".suburbLabel")
+      .data(suburbData.features)
+    .enter().append("p")
+      .classed("suburbLabel", true)
+      .text(function(d) { return d.properties.sa2; });
+
   regions = regionGroup
     .selectAll(".region")
       .data(regionData.features)
@@ -76,14 +83,32 @@ function calculations() {
       return i / (abcColours.length - 1) * (abcColourRange[1] - abcColourRange[0]) + abcColourRange[0];
     }))
     .interpolate(d3.interpolateRgb);
+
+  abcColours.forEach(function(d, i) {
+    gradient.append("stop")
+      .attr("stop-color", d)
+      .attr("offset", (i / (abcColours.length - 1) * 100) + "%");
+  });
+
+  scale = d3.scaleLinear()
+    .domain(abcColourRange);
+  axis = d3.axisLeft(scale)
+    .tickSizeOuter(0)
+    .ticks(5, ".0%");
 }
 
 function resize() {
   console.log("call resize()");
 
   [width, height, mobile] = getDimensions();
+
   touchScreen.attr("width", width)
     .attr("height", height);
+
+  mobile ?
+    scale.range([200, 0]) :
+    scale.range([250, 0]);
+
   adjustProjection();
   plotMap();
 }
@@ -105,15 +130,52 @@ function adjustProjection() {
 
   // map projection
   mobile ?
-    projection.fitExtent([[10, 30], [width * .8, height - 75]], zoomData) :
-    projection.fitExtent([[0, 35], [width * .8, height - 60]], zoomData);
+    margin = { top: 30, right: width * .2, bottom: 75, left: 10 } :
+    margin = { top: 35, right: width * .2, bottom: 60, left: 10 };
+
+    projection.fitExtent([[margin.left, margin.top], [width - margin.right, height - margin.bottom]], zoomData);
 }
 
 function plotMap() {
   console.log("call plotMap()");
 
   mapGroup.selectAll("path")
-    .attr("d", path);
+    .transition()
+      .duration(500)
+      .attr("d", path);
+
+  suburbLabels.each(function(d) {
+    let coordinates = projection(d.properties.centroid);
+    let labelWidth = parseFloat(d3.select(this).style("width"));
+    let labelHeight = parseFloat(d3.select(this).style("height"));
+    d3.select(this).transition()
+      .duration(500)
+        .style("top", (coordinates[1] - labelHeight / 2) + "px")
+        .style("left", (coordinates[0] - labelWidth / 2) + "px");
+  });
+
+  legendLabel.attr("x", function() {
+      return mobile ? 30 : 40;
+    })
+    .attr("y", -10);
+
+  legendBar.attr("x", function() {
+      return mobile ? width - 40 : width - 50;
+    })
+    .attr("y", function() {
+      return mobile ? height - margin.bottom - 210 : height - margin.bottom - 260;
+    })
+    .attr("width", function() {
+      return mobile ? 30 : 40;
+    })
+    .attr("height", function() {
+      return mobile ? 200 : 250;
+    });
+
+  axisGroup.attr("transform", function() {
+      return mobile ? "translate(" + (width - 40) + ", " + (height - margin.bottom - 210) + ")" : "translate(" + (width - 50) + ", " + (height - margin.bottom - 260) + ")";
+    })
+    .call(axis);
 }
 
 function openViz() {
@@ -165,6 +227,7 @@ function changeLabel(text) {
 
 function regionZoom(event, d) {
   console.log("call regionZoom()");
+  tip.style("opacity", 0);
 
   changeLabel(d.properties.sa3);
 
@@ -181,16 +244,19 @@ function regionZoom(event, d) {
     }
   });
 
-  adjustProjection();
+  suburbLabels.each(function(e) {
+    if (e.properties.sa3 == d.properties.sa3) {
+      d3.select(this).classed("zoom", true);
+    }
+  });
 
-  mapGroup.transition()
-    .duration(500)
-    .selectAll("path")
-    .attr("d", path);
+  adjustProjection();
+  plotMap();
 }
 
 function regionBack() {
   console.log("call regionBack()");
+  tip.style("opacity", 0);
 
   if (zoom) {
     changeLabel((mobile ? "touch region for details" : "click region for details"));
@@ -200,17 +266,41 @@ function regionBack() {
     regions.classed("zoom", false);
     suburbs.classed("zoom", false)
       .classed("fade", false);
+    suburbLabels.classed("zoom", false);
 
     adjustProjection();
-
-    mapGroup.transition()
-    .selectAll("path")
-    .attr("d", path);
+    plotMap();
   }
 }
 
 function suburbInfo(event, d) {
   console.log("call suburbInfo()");
 
-  console.log(d.properties);
+  if (d.properties.value) {
+    tip.style("opacity", 0)
+      .style("top", "0px")
+      .style("left", "0px")
+      .html("<h1>" + d.properties.sa2 + "</h1>"
+        + "<p>2021 price: <span>" + d3.format("$,.0f")(d.properties.value) + "</span></p>"
+        + "<p>2019 price: <span>" + (d.properties.old ? d3.format("$,.0f")(d.properties.old) : "n/a") + "</span></p>"
+        + "<p>growth: <span>" + (d.properties.change ? d.properties.change > 0 ? "+" + d3.format(".1%")(d.properties.change) : d3.format(".1%")(d.properties.change) : "n/a") + "</span></p>");
+
+    let tipWidth = parseFloat(tip.style("width"));
+    let tipHeight = parseFloat(tip.style("height"));
+
+    let coordinates = projection(d.properties.centroid);
+    tip.style("top", function() {
+        return coordinates[1] + 20 + tipHeight > height - margin.bottom ?
+          (coordinates[1] - tipHeight - 20) + "px" :
+          (coordinates[1] + 10) + "px";
+      })
+      .style("left", function() {
+        return coordinates[0] - tipWidth / 2 - 5 < margin.left ?
+          margin.left + "px" :
+          coordinates[0] + tipWidth / 2 + 5 > width - margin.right ?
+          (width - margin.right - tipWidth - 5) + "px" :
+          (coordinates[0] - tipWidth / 2) + "px";
+      })
+      .style("opacity", 1);
+  }
 }
